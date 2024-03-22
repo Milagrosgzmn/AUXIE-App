735b4212d0c0b0b0195c568362461989f1ff05d4
milagros <miliguzman0@gmail.com>
2024-03-22 18:21:39 -0300

    fix: deleted vh propertiesin styling

--- Client/src/components/footer/Footer.jsx
+++ Client/src/components/footer/Footer.jsx
@@ -6,67 +6,79 @@ import { useSelector } from 'react-redux'
 const Footer = ({ myRef3 }) => {
     const nightMode = useSelector(state => state.nightMode)
     return (
-        <footer ref={myRef3} className={nightMode ? 'w-full bg-rgb(10,11,37) p-8' : 'w-full bg-white p-8'}>
-            <hr className='my-8 border-blue-gray-50' />
+        <footer ref={myRef3} className={nightMode ? 'w-full bg-rgb(10,11,37) py-4 px-8' : 'w-full py-4 px-8'}>
+            <hr className='my-8 border-blue-gray-500' />
             <div
                 className={
                     nightMode
-                        ? 'flex items-center justify-between gap-y-6 gap-x-12 bg-rgb(10,11,37) text-center '
-                        : 'flex items-center justify-between gap-y-6 gap-x-12 bg-white text-center '
+                        ? 'flex items-center justify-evenly bg-rgb(10,11,37) text-center '
+                        : 'flex items-center justify-evenly text-center '
                 }
             >
-                <img src={CircleIconAuxie} alt='logo-ct' className='w-[150px]' />
-                <ul className='flex items-center gap-y-2 gap-x-8'>
+                <img src={CircleIconAuxie} alt='logo-ct' className='w-24' />
+                <ul className='flex flex-col sm:flex-row justify-between'>
                     <Link to={'/aboutUs'}>
-                        <li>
+                        <li className='px-2 text-left sm:text-center'>
                             <Typography
                                 color='blue-gray'
-                                className='font-normal transition-colors hover:text-blue-500 focus:text-blue-500'
+                                className='font-normal
+                                text-sm
+                                transition-colors hover:text-blue-500 focus:text-blue-500'
                             >
                                 Sobre nosotros
                             </Typography>
                         </li>
                     </Link>
                     <Link to={'/guarantee'}>
-                        <li>
+                        <li className='px-2 text-left sm:text-center'>
                             <Typography
                                 color='blue-gray'
-                                className='font-normal transition-colors hover:text-blue-500 focus:text-blue-500'
+                                className='font-normal 
+                                text-sm
+                                transition-colors hover:text-blue-500 focus:text-blue-500'
                             >
                                 Garantías
                             </Typography>
                         </li>
                     </Link>
                     <Link to='/help'>
-                        <li>
+                        <li className='px-2 text-left sm:text-center'>
                             <Typography
                                 color='blue-gray'
-                                className='font-normal transition-colors hover:text-blue-500 focus:text-blue-500'
+                                className='font-normal 
+                                text-sm
+                                transition-colors hover:text-blue-500 focus:text-blue-500'
                             >
                                 Como funciona
                             </Typography>
                         </li>
                     </Link>
                     <Link to='/support'>
-                        <li>
+                        <li className='px-2 text-left sm:text-center'>
                             <Typography
                                 color='blue-gray'
-                                className='font-normal transition-colors hover:text-blue-500 focus:text-blue-500'
+                                className='font-normal
+                                text-sm
+                                transition-colors hover:text-blue-500 focus:text-blue-500'
                             >
                                 Contacto
                             </Typography>
                         </li>
                     </Link>
                 </ul>
-                <div className='flex flex-column justify-center text-center font-normal'>
-                    <Place />
-                    <label>Argentina</label>
-
-                    <Place />
-                    <label>México</label>
-                </div>
             </div>
+            <div className='flex justify-evenly justify-center text-center font-normal'>
+                    <div className='flex'>
+                        <Place />
+                        <label>Argentina</label>
+                    </div>
+                    <div  className='flex'>
+                        <Place />
+                        <label>México</label>
+                    </div>
 
+                    
+                </div>
             <div className='flex items-center justify-center'>
                 <Typography color='blue-gray' className='text-center font-normal '>
                     &copy; 2023 Auxie
