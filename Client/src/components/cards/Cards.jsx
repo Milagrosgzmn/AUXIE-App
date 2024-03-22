f5aa651e69fe18afe89441d69f0c354053aab4cb
milagros <miliguzman0@gmail.com>
2024-03-22 18:21:26 -0300

    fix: cards styles

--- Client/src/components/cards/Cards.jsx
+++ Client/src/components/cards/Cards.jsx
@@ -1,4 +1,3 @@
-import style from './cards.module.scss'
 
 //* Components
 import Card from '../card/Card'
@@ -12,30 +11,28 @@ const Cards = () => {
     const { currentPageData } = usePagination(12, users)
 
     return (
-        <>  
-            <div className={style.cont}>
-            <div className={style.cards}>
-                {currentPageData &&
-                    currentPageData.map((user) => (
-                        <Card
-                            key={user.id}
-                            id={user.id}
-                            googleId={user.googleId}
-                            userUid={user.userUid}
-                            firstName={user.firstName}
-                            lastName={user.lastName}
-                            services={user.services}
-                            averageRating={user.averageRating}
-                            completedWorks={user.completedWorks}
-                            image={user.image.secure_url}
-                        />
-                    ))}
-            </div>
-            <div className={style.pagination}>
-                <Pagination num={12} data={users} />
-            </div>
-            </div>
-        </>
+            <section className='flex flex-col justify-center items-center ml-4'>
+                <div className='mb-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 px-4'>
+                    {currentPageData &&
+                        currentPageData.map((user) => (
+                            <Card
+                                key={user.id}
+                                id={user.id}
+                                googleId={user.googleId}
+                                userUid={user.userUid}
+                                firstName={user.firstName}
+                                lastName={user.lastName}
+                                services={user.services}
+                                averageRating={user.averageRating}
+                                completedWorks={user.completedWorks}
+                                image={user.image.secure_url}
+                            />
+                        ))}
+                </div>
+                <div className='mt-8'>
+                    <Pagination num={12} data={users} />
+                </div>
+            </section>
     )
 }
 
